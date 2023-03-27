35ac039eafc786e8928c9ef94e6b2208db222038
Burak Temelkaya <burak_istanbul_07@hotmail.com>
2023-03-28 00:13:21 +0300

    16. Ders tamamlandı
    Proje dosyaları oluşturuldu.

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = 'northwind';
+  user: string = 'Burak Temelkaya';
+
+}
+
+//component htmlin datasını yönettiğimiz yerdir
